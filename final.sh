6df17bf3532ae00bc692c19cd60e5eb46fb380f1
Diogo Aluai <diogo.aluai@mgm-tp.com>
2025-08-17 11:13:03 +0100

    Update final with lib export option

--- final.sh
+++ final.sh
@@ -24,6 +24,20 @@ sdk use java "$java_8_sdkman_id" || {
   exit 1
 }
 
-make clean
-make
+make clean || exit 1
+make || exit 1
 
+echo "Build done successfully"
+echo ""
+read -p "Do you want to copy lib/libjcurses.so to /usr/lib/? (y/n) " answer
+
+if [[ "$answer" =~ ^[Yy]$ ]]; then
+    echo -n "Copying... "
+    sudo cp lib/libjcurses.so /usr/lib/
+    echo "Done"
+elif [[ "$answer" =~ ^[Nn]$ ]]; then
+    echo "Not doing it then"
+else
+    echo "I don't know what you mean"
+    echo "Here's the command: 'sudo cp lib/libjcurses.so /usr/lib/'"
+fi
